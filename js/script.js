2884ce79fa0a2edd6afe51ab71d80da8b1707d4a
Juniorawan06 <haseebawan.1560@gmail.com>
2023-05-19 18:50:38 +0500

    added particles animations

--- js/script.js
+++ js/script.js
@@ -14,3 +14,62 @@ window.onscroll = function () {
     burger.style.display = "none";
   }
 };
+
+const particles = document.querySelectorAll(".particle-wrapper > div");
+
+function handleMouseMovement(event) {
+  const mouseX = (event.clientX / window.innerWidth) * 100;
+  const mouseY = (event.clientY / window.innerHeight) * 100;
+
+  particles.forEach((particle, index) => {
+    particle.style.transition = "all 0.5s ease";
+    particle.style.transform = `translate3d(${mouseX * 0.4}px, ${
+      mouseY * 0.4
+    }px, 0) scale3d(1, 1, 1) rotate(${mouseX}deg)`;
+  });
+}
+
+function handleScroll() {
+  const scrollY = window.scrollY || window.pageYOffset;
+  const particleTop = document.querySelectorAll(
+    ".particle-wrapper .particle-1,.particle-8"
+  );
+
+  particleTop.forEach((particle) => {
+    particle.style.transform = `translate3d(${scrollY * 0.4}px, ${
+      scrollY * -0.4
+    }px, 0) rotate(${scrollY}deg)`;
+  });
+  const particleRight = document.querySelectorAll(
+    ".particle-wrapper .particle-6,.particle-7"
+  );
+
+  particleRight.forEach((particle) => {
+    particle.style.transform = `translate3d(${scrollY * 0.4}px, ${
+      scrollY * 0.4
+    }px, 0) rotate(${scrollY}deg)`;
+  });
+
+  const particleLeft = document.querySelectorAll(
+    ".particle-wrapper .particle-3,.particle-4"
+  );
+
+  particleLeft.forEach((particle) => {
+    particle.style.transform = `translate3d(${scrollY * -0.4}px, ${
+      scrollY * -0.4
+    }px, 0) rotate(${scrollY}deg)`;
+  });
+
+  const particleBottom = document.querySelectorAll(
+    ".particle-wrapper .particle-2,.particle-5"
+  );
+
+  particleBottom.forEach((particle) => {
+    particle.style.transform = `translate3d(${scrollY * -0.4}px, ${
+      scrollY * 0.4
+    }px, 0) rotate(${scrollY}deg)`;
+  });
+}
+
+window.addEventListener("mousemove", handleMouseMovement);
+window.addEventListener("scroll", handleScroll);
